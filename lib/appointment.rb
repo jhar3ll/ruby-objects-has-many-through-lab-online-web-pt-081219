a1feddfa1f6f435a7cd123ef418a8464dbdba425
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-03 01:41:08 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,7 +2,7 @@ class Appointment
   attr_accessor :date, :patient, :doctor 
   @@all = []
   
-  def initialize(name)
+  def initialize(date, patient, doctor)
     @date = date  
     @patient = patient
     @doctor = doctor
